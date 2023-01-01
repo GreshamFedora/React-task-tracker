ac9f93f85c0a46125c05badfda0caac4fd86b393
Gresham Fedora <gresham.fedora@gmail.com>
2022-12-31 20:18:54 -0500

    tasks component and test data added

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,24 @@
-const tasks
+const tasks = [
+    {
+    id: 1,
+    test:'Doctor Appointment',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true,
+    },
+    {
+    id: 2,
+    test:'Meeting at School',
+    day: 'Feb 6th at 1:30pm',
+    reminder: true,
+    },
+    {
+    id: 3,
+    test:'Food Shopping',
+    day: 'Feb 5th at 2:30pm',
+    reminder: false,
+    },
+]
+
 const Tasks = () => {
   return (
     <div>Tasks</div>
