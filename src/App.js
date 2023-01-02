fa79b67daa37cb5b171ac5f2f1b0cfe793126122
Gresham Fedora <gresham.fedora@gmail.com>
2023-01-01 21:52:59 -0500

    outputting tasks to page and tasks relocated to be global

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import Header from './components/Header'
 import Exp from './components/Exp'
+import Tasks from './components/Tasks'
+import { useState } from "react"
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+    id: 1,
+    text:'Doctor Appointment',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true,
+    },
+    {
+    id: 2,
+    text:'Meeting at School',
+    day: 'Feb 6th at 1:30pm',
+    reminder: true,
+    },
+    {
+    id: 3,
+    text:'Food Shopping',
+    day: 'Feb 5th at 2:30pm',
+    reminder: false,
+    },
+])
+
   const name = 'Maak'
   return (
     <div className="container">
         
         <Header title = "daBomb is garbage sauce"/>
-        
+        <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,13 @@
-const tasks = [
-    {
-    id: 1,
-    test:'Doctor Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-    },
-    {
-    id: 2,
-    test:'Meeting at School',
-    day: 'Feb 6th at 1:30pm',
-    reminder: true,
-    },
-    {
-    id: 3,
-    test:'Food Shopping',
-    day: 'Feb 5th at 2:30pm',
-    reminder: false,
-    },
-]
+const Tasks = ({tasks}) => {
 
-const Tasks = () => {
   return (
-    <div>Tasks</div>
+    <>      
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
   )
 }
 
 export default Tasks
+ 
