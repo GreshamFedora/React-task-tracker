75b3a5c65555b745547d93e5077f033ad6a7a46a
Gresham Fedora <gresham.fedora@gmail.com>
2023-01-17 12:43:07 -0500

    json server set up and tasks all updated or added to work w backend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
@@ -6,46 +6,88 @@ import AddTask from './components/AddTask'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-    id: 1,
-    text:'Doctor Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-    },
-    {
-    id: 2,
-    text:'Meeting at School',
-    day: 'Feb 6th at 1:30pm',
-    reminder: true,
-    },
-    {
-    id: 3,
-    text:'Food Shopping',
-    day: 'Feb 5th at 2:30pm',
-    reminder: false,
-    },
-])
+  const [tasks, setTasks] = useState([])
+
+useEffect(() => {
+  const getTasks = async () => {
+    const tasksFromServer = await fetchTasks()
+    setTasks(tasksFromServer)
+
+  }
+
+  getTasks()
+},[])
+
+// Fetch Tasks from back end -- this was return 2x
+const fetchTasks = async () => {
+  const res = await fetch('http://localhost:5000/tasks')
+  const data = await res.json()
+
+  return data
+}
+
+// this one updates reminder toggle --> back end
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+
+  return data
+}
 
 // Add Task
-const addTask = (task) => {
-  // db will create own
+
+const addTask = async  (task) => {
+const res = await fetch('http://localhost:5000/tasks', {
+method: 'POST',
+headers: {
+  'Content-type': 'application/json'
+  },
+  body: JSON.stringify(task)
+})
+
+//new task added = data returned and this is a promise so we have to await
+const data = await res.json()
+//adding new task to current tasks
+setTasks([...tasks, data])
+  /*
+//上面的代碼替代下面的
+  // db will create own so w/ back end it's commented out
   const id = Math.floor(Math.random() * 10000) + 1
 const newTask = { id,...task }
 setTasks([...tasks, newTask])
-  
+*/
+
+
 }
 
 // Delete Task
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  // don't need to save in a variable as we're not getting data back
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE'
+  })
+
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
 // Toggle Reminder
-const toggleReminder = (id) => {
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+  
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+  
   setTasks(
     tasks.map((task) => 
-    task.id === id ? {...task, reminder: !task.reminder} : task))
+    task.id === id ? {...task, reminder: data.reminder} : task))
 }
 
   return (
